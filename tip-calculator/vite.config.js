075effc76a49e549d31f12f546086b993a7aa71b
Gordon_Palagi <89727193+GordonPalagi@users.noreply.github.com>
2023-11-24 13:35:06 -0800

    vite.config base change

--- tip-calculator/vite.config.js
+++ tip-calculator/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindConfig from './src/tailwind.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/tip-calculator/'
 })
